a18b475a5db353c650b3a01540e51fb35c714f33
yifan zhou <yfzhou.cs@gmail.com>
2019-01-22 20:03:45 +0800

    setup.py and __init__.py added

---
+++ __init__.py
@@ -0,0 +1,1 @@
+name = "xml2pytorch"
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xml2pytorch",
+    version="0.0.1",
+    author="Yifan Zhou",
+    author_email="yfzhou.cs@gmail.com",
+    description="use xml to define pytorch neural networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yfzhoucs/xml2pytorch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
